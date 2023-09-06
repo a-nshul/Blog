f5fa3ae9150193170bf6c82fb25488f35abd6d55
Anshul upadhyay <82251676+a-nshul@users.noreply.github.com>
2023-09-06 11:36:28 +0530

    Update BlogList.js

--- Backend_blog/Frontend code/dynamic_blog_page1/src/components/BlogList.js
+++ Backend_blog/Frontend code/dynamic_blog_page1/src/components/BlogList.js
@@ -7,7 +7,7 @@ const BlogList = () => {
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
-        const response = await axios.get('http://localhost:3003/blogs');
+        const response = await axios.get('https://blog-fmpk-a-nshul.vercel.app/auth/blogs');
         setBlogs(response.data);
       } catch (error) {
         console.error('Error fetching blogs:', error);
